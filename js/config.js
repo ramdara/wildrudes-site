d0e8cfa34bc52acfa4c90343dd6f50ffe0b832ed
ABHIRAM DARA <abhiram1383@gmail.com>
2025-04-30 21:10:05 -0700

    changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_Ozyk733Ji', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '20feriftihs73173460fmkp415', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_wTACA1zaW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5jfska8mb77u22897is6h7resu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
